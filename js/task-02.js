5dd2ae5184dc1494cb86bab44d163a8df9e1839b
MikolajBajdalski <48803808+MikolajBajdalski@users.noreply.github.com>
2023-07-27 20:20:54 +0200

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listOfIngredients = document.querySelector("#ingredients");
+
+ingredients.forEach((item) => {
+  const listItem = document.createElement("li");
+  listItem.textContent = item;
+  listItem.classList.add("item");
+  listOfIngredients.appendChild(listItem);
+});
+
+console.log(listOfIngredients);
