75556899ab965f112e75a9a36f435c6b00aa93d6
MikolajBajdalski <48803808+MikolajBajdalski@users.noreply.github.com>
2023-08-03 19:00:04 +0200

    task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,41 @@
+const inputElement = document.querySelector("#controls input");
+const createButton = document.querySelector("#controls [data-create]");
+const destroyButton = document.querySelector("#controls [data-destroy]");
+const boxesArea = document.querySelector("#boxes");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function returnNumber() {
+  return parseInt(inputElement.value);
+}
+
+function createBoxes(amount) {
+  console.log("poszło");
+  let widthAndHeight = 30;
+
+  for (let i = 0; i <= amount; i += 1) {
+    const newDiv = document.createElement("div");
+    newDiv.style.width = `${widthAndHeight}px`;
+    newDiv.style.height = `${widthAndHeight}px`;
+    newDiv.style.backgroundColor = getRandomHexColor();
+    boxesArea.appendChild(newDiv);
+    widthAndHeight += 10;
+  }
+}
+
+function destroyBoxes() {
+  while (boxesArea.firstChild) {
+    boxesArea.removeChild(boxesArea.firstChild);
+  }
+}
+
+createButton.addEventListener("click", function () {
+  const numberOfBoxes = returnNumber();
+  createBoxes(numberOfBoxes);
+});
+
+destroyButton.addEventListener("click", destroyBoxes);
