2e5128b8f7ae321875b7742bc70e1932e30dec30
MikolajBajdalski <48803808+MikolajBajdalski@users.noreply.github.com>
2023-08-03 18:12:48 +0200

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const loginForm = document.querySelector(".login-form");
 
+const handleSubmit = (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(loginForm);
+  const formValues = {};
+
+  let allFieldsFilled = true;
+  for (const [name, value] of formData.entries()) {
+    if (!value) {
+      allFieldsFilled = false;
+      break;
+    }
+    formValues[name] = value;
+  }
+
+  if (allFieldsFilled) {
+    console.log("Form values:", formValues);
+    loginForm.reset();
+  } else {
+    alert("Wszystkie pola powinny być wypełnione!");
+  }
+};
+
+loginForm.addEventListener("submit", handleSubmit);
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function changeBackgroundColorBody() {
+  body.style.backgroundColor = getRandomHexColor();
+  hexColorSpan.textContent = getRandomHexColor();
+}
+
+const body = document.body;
+const button = document.querySelector(".change-color");
+const hexColorSpan = document.querySelector(".color");
+
+button.addEventListener("click", changeBackgroundColorBody);
